5eb8e1eb168d9fa1768ac6cb8cbe6a7ecd80efcc
储文青 <571869993@qq.com>
2018-07-11 18:34:27 -0700

    adapt to pytorch0.4.0

--- encoding/dilated/resnet_syncbn.py
+++ encoding/dilated/resnet_syncbn.py
@@ -15,7 +15,6 @@ model_urls = {
     'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
 }
 
-affine_par = True
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
@@ -32,13 +31,11 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
                                padding=dilation, dilation=dilation, bias=False)
-        #self.bn1 = norm_layer(planes)
-        self.bn1 = nn.BatchNorm2d(planes, affine = affine_par)
+        self.bn1 = norm_layer(planes)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
                                padding=previous_dilation, dilation=previous_dilation, bias=False)
-        #self.bn2 = norm_layer(planes)
-        self.bn2 = nn.BatchNorm2d(planes, affine = affine_par)
+        self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
 
@@ -70,25 +67,14 @@ class Bottleneck(nn.Module):
                  downsample=None, previous_dilation=1, norm_layer=None):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        #self.bn1 = norm_layer(planes)
-        self.bn1 = nn.BatchNorm2d(planes,affine = affine_par)
-        for i in self.bn1.parameters():
-            i.requires_grad = False
+        self.bn1 = norm_layer(planes)
         self.conv2 = nn.Conv2d(
             planes, planes, kernel_size=3, stride=stride,
             padding=dilation, dilation=dilation, bias=False)
-        #self.bn2 = norm_layer(planes)
-        self.bn2 = nn.BatchNorm2d(planes,affine = affine_par)
-        for i in self.bn2.parameters():
-            i.requires_grad = False
-
+        self.bn2 = norm_layer(planes)
         self.conv3 = nn.Conv2d(
             planes, planes * 4, kernel_size=1, bias=False)
-        #self.bn3 = norm_layer(planes * 4)
-        self.bn3 = nn.BatchNorm2d(planes * 4, affine = affine_par)
-        for i in self.bn3.parameters():
-            i.requires_grad = False
-
+        self.bn3 = norm_layer(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.dilation = dilation
@@ -154,11 +140,7 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
-        #self.bn1 = norm_layer(64)
-        self.bn1 = nn.BatchNorm2d(64, affine = affine_par)
-        for i in self.bn1.parameters():
-            i.requires_grad = False
-
+        self.bn1 = norm_layer(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0], norm_layer=norm_layer)
@@ -180,12 +162,9 @@ class ResNet(nn.Module):
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
-            #elif isinstance(m, norm_layer):
-            elif isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, norm_layer):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
-                for i in m.parameters():
-                    i.requires_grad = False
 
     def _make_layer(self, block, planes, blocks, stride=1, dilation=1, norm_layer=None):
         downsample = None
@@ -193,12 +172,8 @@ class ResNet(nn.Module):
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
-                #norm_layer(planes * block.expansion),
-                nn.BatchNorm2d(planes * block.expansion,affine = affine_par),
+                norm_layer(planes * block.expansion),
             )
-
-        for i in downsample._modules['1'].parameters():
-            i.requires_grad = False
 
         layers = []
         if dilation == 1 or dilation == 2:
@@ -282,7 +257,6 @@ def resnet101(pretrained=False, root='~/.encoding/models', **kwargs):
     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
     if pretrained:
         from ..models.model_store import get_model_file
-        print('resnet')
         model.load_state_dict(torch.load(
             get_model_file('resnet101', root=root)), strict=False)
     return model
@@ -297,6 +271,7 @@ def resnet101coco(pretrained=False, root='~/.encoding/models', **kwargs):
     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
     if pretrained:
         from ..models.model_store import get_model_file
+        root = os.path.expanduser(root)
         model.load_state_dict(torch.load(os.path.join(root, 'MS_DeepLab_resnet_pretrained_COCO_init.pth')), strict=False)
 
     return model
